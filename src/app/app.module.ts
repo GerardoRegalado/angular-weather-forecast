2f9c21eea0f321f8b4ce4a402b3aadb056e8cd58
Gerardo Regalado <gerardo.regalado24@gmail.com>
2023-12-15 16:40:44 -0600

    Feat(): Created components and service files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { WeatherComponent } from './components/weather/weather.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    WeatherComponent
   ],
   imports: [
     BrowserModule,

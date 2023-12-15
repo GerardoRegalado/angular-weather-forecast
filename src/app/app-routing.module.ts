c5913a7001e45617d7a968dc830b2a39f442944e
Gerardo Regalado <gerardo.regalado24@gmail.com>
2023-12-15 16:47:35 -0600

    Feat(): Removing default data and established new routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { WeatherComponent } from './components/weather/weather.component';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'weather/:location', component: WeatherComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

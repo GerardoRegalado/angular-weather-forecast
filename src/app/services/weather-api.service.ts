72cf3dee18b1fa9bbb47766a09831c10b5a3ccf9
Gerardo Regalado <gerardo.regalado24@gmail.com>
2023-12-15 18:53:29 -0600

    Feat(): Created weather service

--- src/app/services/weather-api.service.ts
+++ src/app/services/weather-api.service.ts
@@ -4,6 +4,16 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class WeatherAPIService {
+  private locationRouteAPI = "https://freegeoip.app.json/"
 
   constructor() { }
+
+  async getLocationData() {
+    const response = await fetch(this.locationRouteAPI);
+    return await response.json();
+    }
+    async getWeatherData (longitude: string, latitude: string) {
+    const response = await fetch(`http://www.7timer.info/bin/civillight.php?lon=${longitude} &lat=${latitude}&ac=8&unit-metric&output=json`);
+    return await response.json();
+    }
 }

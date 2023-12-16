0f062d0da00470d6744960b9b99afab2f5d979df
Gerardo Regalado <gerardo.regalado24@gmail.com>
2023-12-16 11:31:04 -0600

    Fix():Changing home component html, scss and logic in order to show just the requested cities

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WeatherAPIService } from '../../services/weather-api.service';
 
 @Component({
   selector: 'awf-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  public cities = [
+    { name: 'Kansas', endpoint: 'TOP', data: {}, forecast: []},
+    { name: 'District of Columbia', endpoint: 'LWX', data: {}, forecast: []}
+  ];
+  constructor(public weatherService: WeatherAPIService){}
+  ngOnInit(): void {
+    this.cities.forEach(city => {
+      this.weatherService.getWeatherData(city.endpoint).subscribe(data => {
+        city.data = data;
+      });
+    });
+    console.log(this.cities.forEach(city => console.log(city.data)))
+  }
+
+
 
 }
